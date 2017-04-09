1c5c9986b9180b4cea126492e09ea6e0c35a450a
Lalit-som <Lalitsom27@gmail.com>
2017-04-09 16:45:57 +0530

    new design

---
+++ header.php
@@ -0,0 +1,29 @@
+<section class="mbr-navbar mbr-navbar--freeze  mbr-navbar--auto-collapse" id="menu-32" >
+    <div class="mbr-navbar__section mbr-section" style="background-color:#283e4a;  box-shadow: 2px 1px 5px #1c1c1c;">
+        <div class="mbr-section__container container">
+            <div class="mbr-navbar__container" >
+                <div class="mbr-navbar__column mbr-navbar__column--s mbr-navbar__brand">
+                    <span class="mbr-navbar__brand-link mbr-brand mbr-brand--inline">
+                        <span class="mbr-brand__logo"><a href="#"><img src="assets/images/icon.png" class="mbr-navbar__brand-img mbr-brand__img" alt="AI Games"></a></span>
+                        <span class="mbr-brand__name"><a class="mbr-brand__name text-white" href="#">Lalit Som</a></span>
+                    </span>
+                </div>
+                <div class="mbr-navbar__hamburger mbr-hamburger"><span class="mbr-hamburger__line"></span></div>
+                <div class="mbr-navbar__column mbr-navbar__menu">
+                    <nav class="mbr-navbar__menu-box mbr-navbar__menu-box--inline-right">
+                        <div class="mbr-navbar__column">
+                            <ul class="mbr-navbar__items mbr-navbar__items--right float-left mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-decorator mbr-buttons--active">
+                               <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="index.php">I am</a></li>
+                               <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#">Skills</a></li>
+                               <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#">Projects</a></li></ul>
+                               <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#">Education</a></li></ul>
+                               <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="#">Achievments</a></li></ul>
+                            <ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-inverse mbr-buttons--active">
+                              <li class="mbr-navbar__item"><a id ="logbtn" class="mbr-buttons__btn btn btn-default" href="#">Contact Me</a></li></ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>LALIT SOM</title>
+   <meta name="title" content="">
+   <meta name="description" content="">
+   <meta name="keywords" content="">
+   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/icon.png" type="image/x-icon">
+
+
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/animate.css/animate.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/socicon.min.css">
+  <link rel="stylesheet" href="assets/mobirise/css/style.css">
+  <link rel="stylesheet" href="assets/dropdown-menu-plugin/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  <link rel="stylesheet" href="assets/main.css" type="text/css">
+
+
+
+
+</head>
+<body style="background-color:#fafafa">
+<?php include 'header.php' ?>
+
+<div id="content1">
+<section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height">
+<div class="mbr-box__container mbr-section__container container">
+
+  <div class="row mbr-box__magnet--center-left" style="margin-top:55px"  >
+    <div class=" col-xs-11 col-sm-6 col-md-6">
+      <div class="mbr-hero" style="margin-left:-60px;">
+         <div class="mbr-overlay" style="opacity: 0.5; background-color: #283e4a; "></div>
+         <figure class="mbr-figure animated fadeIn"><img src="assets/images/image_me.jpg" class="mbr-figure__img"></figure>
+     </div>
+
+  </div>
+    <div class=" col-xs-11 col-sm-6 col-md-6" style="margin-top:30px">
+      <div class="mbr-hero animated fadeInUp " style="background-color:#283e4a; padding:10px;">
+          <h1 class="mbr-hero__text">Hey, I am Lalit Som.</h1></br></br>
+          <p class="mbr-hero__subtext well" style="padding:5px; font-size:20px; letter-spacing:3px; word-spacing:3px; color:black;"><strong>
+            I am a Computer Science Student, trying to learn as much as I can in the field of Software and Web Development.</br>
+            Learning new Frameworks, Technologies and Languages every day.
+
+            </strong><br></p>
+      </div>
+  </div></div>
+
+</div>
+</section>
+
+
+
+
+
+<section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height" id="next_div" style="margin-top:70px; ">
+  <div class="mbr-box__container mbr-section__container container ">
+    <div class="mbr-hero" style="background-color:#283e4a; padding:10px;">
+       <h1 class="mbr-hero__text"> Languages and Techologies I have Worked with.</h1>
+    </div>
+
+    <div class="row mbr-box__magnet--center-left well" >
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/html.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">HTML5</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/js.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">JAVASRIPT</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/css.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">CSS</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/android.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">Android</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/bootstrap.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">Bootstrap</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/c.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">C</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/cpp.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">C++</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/csharp.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">.Net-C#</div>
+               </div>
+             </figure>
+
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/java.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">Java</div>
+               </div>
+             </figure>
+
+      </div>
+
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/jquery.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">Jquery</div>
+               </div>
+             </figure>
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/unity.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">Unity</div>
+               </div>
+             </figure>
+      </div>
+
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/php.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">PHP</div>
+               </div>
+             </figure>
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/python.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">Python</div>
+               </div>
+             </figure>
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px; ">
+           <figure class="mbr-figure animated fadeIn container_o"><img style ="max-height:140px;" src="assets/images/things/sql.png" class="mbr-figure__img image_o">
+             <div class="overlay_o">
+                 <div class="text_o">SQL</div>
+               </div>
+             </figure>
+      </div>
+
+
+
+
+  </div>
+
+  </div>
+</section>
+
+
+
+
+
+<section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height" id="next_div" style="margin-top:70px; ">
+  <div class="mbr-box__container mbr-section__container container ">
+    <div class="mbr-hero" style="background-color:#283e4a; padding:10px;">
+       <h1 class="mbr-hero__text">Find Me on Web</h1>
+    </div>
+    <div class="row mbr-box__magnet--center-left well" >
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px;">
+        <div class="mbr-hero">
+           <figure class="mbr-figure animated fadeIn"><a target="_blank" href="https://www.linkedin.com/in/lalitsom" Title="LinkedIn"><img style ="max-height:100px;" src="assets/images/contact/linkedin.png" class="mbr-figure__img"></a></figure>
+       </div>
+      </div>
+
+      <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px;">
+        <div class="mbr-hero" >
+           <figure class="mbr-figure animated fadeIn"><a target="_blank" href="mailto:lalitsom27@gmail.com" Title="lalitsom27@gmail.com"><img style ="max-height:100px;" src="assets/images/contact/gmail.png" class="mbr-figure__img"></a></figure>
+       </div>
+    </div>
+
+    <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px;">
+      <div class="mbr-hero" >
+         <figure class="mbr-figure animated fadeIn"><a target="_blank" href="https://plus.google.com/u/1/117338808444614294269" Title="Google+"><img style ="max-height:100px;" src="assets/images/contact/gplus.png" class="mbr-figure__img"></a></figure>
+     </div>
+  </div>
+
+
+  <div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px;">
+    <div class="mbr-hero" >
+       <figure class="mbr-figure animated fadeIn"><a target="_blank" href="https://facebook.com/BDOUF" Title="Facebook"><img style ="max-height:100px;" src="assets/images/contact/fb.png" class="mbr-figure__img"></a></figure>
+   </div>
+</div>
+
+
+<div class=" col-xs-11  col-sm-2 col-md-2" style=" margin-left:100px; margin-top:30px;">
+  <div class="mbr-hero" >
+     <figure class="mbr-figure animated fadeIn"><a target="_blank" href="https://github.com/lalitsom" Title="GitHub"><img style ="max-height:100px;" src="assets/images/contact/github.png" class="mbr-figure__img"></a></figure>
+ </div>
+</div>
+
+
+
+  </div>
+
+  </div>
+  <div id="empty_space" style="width:100%; height:60px; ">
+
+  </div>
+</section>
+
+
+<?php include 'footer.php' ?>
+
+
+
+
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/SmoothScroll.js"></script>
+  <script src="assets/jarallax/jarallax.js"></script>
+  <script src="assets/mobirise/js/script.js"></script>
+  <script src="assets/dropdown-menu-plugin/script.js"></script>
+
+
+</body>
+</html>
